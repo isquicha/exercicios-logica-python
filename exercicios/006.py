6737f841679938d39915e31dc2d75b1510c577bb
Andre Vicente <isquicha@gmail.com>
2020-11-02 02:10:33 -0300

    Add exercicio 6

---
+++ exercicios/006.py
@@ -0,0 +1,7 @@
+"""
+Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+"""
+pi = 3.1415926
+raio = float(input("Digite o raio do círculo: "))
+area = pi * (raio ** 2)
+print(f"A área de um círculo de raio {raio} é {area:.2f}m².")
