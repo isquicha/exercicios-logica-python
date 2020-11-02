952f4d8df29dfccd4c42107ac52b3314561a71b9
Andre Vicente <isquicha@gmail.com>
2020-11-02 02:10:33 -0300

    Add exercicio 5

---
+++ exercicios/005.py
@@ -0,0 +1,6 @@
+"""
+Faça um Programa que converta metros para centímetros.
+"""
+metros = float(input("Digite o valor em metros: "))
+centimetros = metros * 100
+print(f"{metros:.2f} metros equivalem a {centimetros:.2f} centimetros.")
