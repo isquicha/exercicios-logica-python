d1dc165248d60f0fa36d586a42beb9b0608e27e1
Andre Vicente <isquicha@gmail.com>
2020-11-04 03:38:35 -0300

    Add exercicio 20

---
+++ exercicios/020.py
@@ -0,0 +1,9 @@
+"""
+Faça um Programa que peça um valor e mostre na tela se o valor é
+positivo ou negativo.
+"""
+valor = float(input("Digite um valor: "))
+if valor > 0:
+    print("O valor é positivo")
+elif valor < 0:
+    print("O valor é negativo")
