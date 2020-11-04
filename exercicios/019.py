b590472a56a70d97b8dfe2d7b425ada2523880ac
Andre Vicente <isquicha@gmail.com>
2020-11-04 03:38:35 -0300

    Add exercicio 19

---
+++ exercicios/019.py
@@ -0,0 +1,9 @@
+"""
+Faça um Programa que peça dois números e imprima o maior deles.
+"""
+num1 = float(input("Digite um numero: "))
+num2 = float(input("Digite outro numero: "))
+if num1 > num2:
+    print(num1)
+else:
+    print(num2)
