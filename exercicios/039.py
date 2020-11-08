46b730b620f7f60390d47a1acb2038a73ff36676
Andre Vicente <isquicha@gmail.com>
2020-11-08 05:11:44 -0300

    Add exercicio 39

---
+++ exercicios/039.py
@@ -0,0 +1,9 @@
+"""
+Faça um Programa que peça um número inteiro e determine se ele é par ou impar.
+Dica: utilize o operador módulo (resto da divisão).
+"""
+numero = int(input("Digite um número inteiro: "))
+if numero % 2 == 0:
+    print("Par")
+else:
+    print("Ímpar")
