be75c76ab2bea68c3c00be58287b14eb723db006
Gustavo Felipe <97261564+gusttavofelipe@users.noreply.github.com>
2022-12-03 07:58:30 -0300

    Update 055.py
    assim mostra os numeros tanto se o 1° for maior quanto 2° tambem

--- exercicios/055.py
+++ exercicios/055.py
@@ -2,7 +2,13 @@
 Faça um programa que receba dois números inteiros e gere os números inteiros
 que estão no intervalo compreendido por eles.
 """
-primeiro_numero = int(input("Digite um numero: "))
-segundo_numero = int(input("Digite um numero: "))
-for i in range(primeiro_numero + 1, segundo_numero):
-    print(i)
+print('\nDigite dois números inteiros a seguir.\n')
+numero1 = int(input('1°: '))
+numero2 = int(input('2°: '))
+
+if numero1 > numero2:
+    for i in range(numero2+1, numero1):
+        print(i)
+else:
+    for i in range(numero1+1, numero2):
+        print(i)
